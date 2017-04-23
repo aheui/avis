0a14c448c1e0b0816244acde0fd2b9bbb4f8545b
JongChan Choi <disjukr@naver.com>
2017-04-24 01:58:36 +0900

    logo & label on shelf

--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -6,7 +6,10 @@ import style from './Shelf.css';
 
 class Shelf extends React.Component {
     render() {
-        return <div className={style.shelf}></div>;
+        return <div className={style.shelf}>
+            <img className={style.logo} src="./aheui.svg" alt="아희"/>
+            <div className={style.label}>AVIS - 잘만든 아희 편집기 &amp; 실행기</div>
+        </div>;
     }
 }
 
