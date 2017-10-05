aac4b3c5ec2044a8c01678a51375a53edfae72d6
JongChan Choi <jong@chan.moe>
2017-10-06 06:00:48 +0900

    primary button & loading indicator

--- src/components/input/Button.tsx
+++ src/components/input/Button.tsx
@@ -1,31 +1,67 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
 
+import FontAwesome from '../FontAwesome';
 import * as style from './Button.css';
 
 
+export type ButtonType = 'default' | 'primary';
+
 interface ButtonProps {
     label: string;
+    type?: ButtonType | null | void;
     disabled?: boolean;
     onClick: () => void;
     className?: string;
 }
 
-export default class extends React.Component<ButtonProps> {
+interface ButtonState {
+    loading: boolean;
+}
+
+export default class extends React.Component<ButtonProps, ButtonState> {
+    unmounted: boolean;
+    componentWillMount() {
+        this.setState({ loading: false });
+    }
+    componentWillUnmount() {
+        this.unmounted = true;
+    }
     render() {
         const {
             label,
+            type,
             disabled,
             onClick,
             className,
         } = this.props;
+        const {
+            loading,
+        } = this.state;
         return <div
-            onClick={() => disabled || onClick()}
+            onClick={async () => {
+                if (disabled) return;
+                try {
+                    this.setState({ loading: true });
+                    await onClick();
+                } finally {
+                    if (this.unmounted) return;
+                    this.setState({ loading: false });
+                }
+            }}
             className={classNames(style.button, {
                 [style.disabled]: disabled,
+                [style.primary]: type === 'primary',
             }, className)}>
             <div className={classNames(style.label)}>
-                { label }
+                {
+                    loading ?
+                    <FontAwesome
+                        className={style.spinner}
+                        icon="circle-o-notch"
+                    /> :
+                    label
+                }
             </div>
         </div>;
     }
