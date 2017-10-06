a0462b0dd9e396fa48c0a914a321f7edb6954f0a
JongChan Choi <jong@chan.moe>
2017-10-06 18:30:15 +0900

    save as gist

--- src/components/SideBar/menu/file/SaveAsGistModal.tsx
+++ src/components/SideBar/menu/file/SaveAsGistModal.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as GitHub from 'github-api';
 
 import {
     connect,
@@ -16,34 +17,86 @@ interface SaveAsGistModalProps {
 }
 
 interface SaveAsGistModalState {
-    phase: 1 | 2;
+    phase: Phase1State | Phase2State;
+}
+
+interface Phase1State {
+    type: 'phase1state';
+    filename: string;
+    description: string;
+    isPublic: boolean;
+}
+
+interface Phase2State {
+    type: 'phase2state';
+    gistId: string;
 }
 
 class SaveAsGistModal extends React.Component<SaveAsGistModalProps, SaveAsGistModalState> {
     componentWillMount() {
-        this.setState({ phase: 1 });
+        this.setState({ phase: {
+            type: 'phase1state',
+            filename: '제목없음.aheui',
+            description: 'avis( http://aheui.github.io/avis/ )에서 작성한 코드입니다.',
+            isPublic: true,
+        } });
     }
     closeModal() {
         const { appState } = this.props;
         appState.setUIOpen('modal.saveAsGist', false);
     }
     renderPhase1() {
+        const { appState } = this.props;
+        const { phase } = this.state;
+        if (phase.type !== 'phase1state') return null;
+        const {
+            filename,
+            description,
+            isPublic,
+        } = phase;
         return <Modal
             title="gist로 저장하기"
             prompt={[
                 ['취소', null, () => this.closeModal()],
-                ['확인', 'primary', () => {
-                    // TODO
-                    return new Promise(() => {});
+                ['확인', 'primary', async () => {
+                    const github = new GitHub();
+                    const gist = github.getGist();
+                    const res = await gist.create({
+                        files: { [filename]: {
+                            content: appState.codeSpace.toString(),
+                        } },
+                        description,
+                        public: isPublic,
+                    });
+                    if (res.status < 300) {
+                        this.setState({ phase: {
+                            type: 'phase2state',
+                            gistId: res.data.id,
+                        } });
+                    } else {
+                        // TODO: 아몰랑
+                        throw res;
+                    }
                 }],
             ]}
-            closeModal={() => this.closeModal()}
-            bodyClassName={style.modalBody}>
+            closeModal={() => this.closeModal()}>
             <Label title="파일 이름">
-                <TextInput value="" onChange={value => console.log(value)}/>
+                <TextInput
+                    value={filename}
+                    onChange={filename => this.setState({ phase: {
+                        ...phase,
+                        filename,
+                    } })}
+                />
             </Label>
             <Label title="설명">
-                <TextInput value="" onChange={value => console.log(value)}/>
+                <TextInput
+                    value={description}
+                    onChange={description => this.setState({ phase: {
+                        ...phase,
+                        description,
+                    } })}
+                />
             </Label>
             <Label title="공개 여부">
                 <Switch
@@ -51,23 +104,48 @@ class SaveAsGistModal extends React.Component<SaveAsGistModalProps, SaveAsGistMo
                     leftValue="public"
                     rightLabel="비공개"
                     rightValue="private"
-                    value="public"
-                    onChange={value => console.log(value)}
+                    value={isPublic ? 'public' : 'private'}
+                    onChange={value => this.setState({ phase: {
+                        ...phase,
+                        isPublic: value === 'public',
+                    } })}
                 />
             </Label>
         </Modal>;
     }
     renderPhase2() {
+        const { phase } = this.state;
+        if (phase.type !== 'phase2state') return null;
+        const {
+            gistId,
+        } = phase;
+        const gistUrl = `https://gist.github.com/anonymous/${ gistId }`;
+        const shareUrl = `${ location.protocol }//${ location.host }${ location.pathname }?content=gist:${ gistId }`;
         return <Modal
             title="gist로 저장하기"
             prompt={[
-                ['닫기', null, () => {}],
+                ['닫기', null, () => this.closeModal()],
             ]}
-            closeModal={() => this.closeModal()}>
+            closeModal={() => this.closeModal()}
+            bodyClassName={style.phase2body}>
+            <Label title="아래의 gist url로 저장되었습니다:">
+                <a href={gistUrl} className={style.link} target="_blank">
+                    { gistUrl }
+                </a>
+            </Label>
+            <Label title="아래의 url을 통해서 avis로 다시 열어볼 수 있습니다:">
+                <a href={shareUrl} className={style.link} target="_blank">
+                    { shareUrl }
+                </a>
+            </Label>
         </Modal>;
     }
     render() {
-        return this.renderPhase1();
+        switch (this.state.phase.type) {
+            case 'phase1state': return this.renderPhase1();
+            case 'phase2state': return this.renderPhase2();
+            default: return null;
+        }
     }
 }
 
