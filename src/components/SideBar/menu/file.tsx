fa1f3b3ae4c4169b2707f6523ab3dba1d7ff0157
JongChan Choi <jong@chan.moe>
2017-10-07 21:45:41 +0900

    new document

--- src/components/SideBar/menu/file.tsx
+++ src/components/SideBar/menu/file.tsx
@@ -59,6 +59,11 @@ class ContentComponent extends React.Component<ContentProps> {
                 open={appState.getUIOpen('file.load')}
                 onBarClick={open => appState.setUIOpen('file.load', !open)}>
                 <MenuButton
+                    label="새 문서"
+                    // TODO: 저장되지 않은 내용이 있을 경우 경고모달 띄우기
+                    onClick={() => appState.init('')}
+                />
+                <MenuButton
                     label="내 컴퓨터에서..."
                     onClick={() => this.fileInput.click()}
                 />
